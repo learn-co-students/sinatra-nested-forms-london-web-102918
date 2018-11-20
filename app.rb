44dccedad1804940b23f9a8033b690372254ec7c
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-20 19:56:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,23 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+    @pirate = Pirate.new(params[:pirate])
+
+    @ships = params[:pirate][:ships].map do |ship_hash|
+      Ship.new(ship_hash)
+    end
+
+    erb :"pirates/show"
+    end
+
   end
 end
